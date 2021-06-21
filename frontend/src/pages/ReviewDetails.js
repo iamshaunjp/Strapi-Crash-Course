9605679d1d387476620439dd9483d7f1323aae6e
Shaun <shaun@thenetninja.co.uk>
2021-06-21 15:07:32 +0100

    lesson-11

--- frontend/src/pages/ReviewDetails.js
+++ frontend/src/pages/ReviewDetails.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import useFetch from '../hooks/useFetch'
+import { useQuery, gql } from '@apollo/client'
+
+const REVIEW = gql`
+  query GetReview($id: ID!) {
+    review(id: $id) {
+      title,
+      body,
+      rating,
+      id
+    }
+  }
+`
 
 export default function ReviewDetails() {
   const { id } = useParams()
-  const { loading, error, data } = useFetch('http://localhost:1337/reviews/' + id)
+  const { loading, error, data } = useQuery(REVIEW, {
+    variables: { id: id }
+  })
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
@@ -13,12 +26,12 @@ export default function ReviewDetails() {
 
   return (
     <div className="review-card">
-      <div className="rating">{data.rating}</div>
-      <h2>{data.title}</h2>
+      <div className="rating">{data.review.rating}</div>
+      <h2>{data.review.title}</h2>
 
       <small>console list</small>
 
-      <p>{data.body}</p>
+      <p>{data.review.body}</p>
     </div>
   )
 }
