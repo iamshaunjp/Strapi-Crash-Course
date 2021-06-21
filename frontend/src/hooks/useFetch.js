9767eb28621748872123023bbb2ff77acf479dad
Shaun <shaun@thenetninja.co.uk>
2021-06-21 13:40:50 +0100

    lesson-6

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+      
+      try {
+        const res = await fetch(url)
+        const reviews = await res.json()
+
+        setData({ reviews });
+        setLoading(false)
+      } catch (error) {
+        setError(error)
+        setLoading(false)
+      }
+    }
+
+    fetchData();
+  }, [url])
+
+  return { loading, error, data }
+}
+
+export default useFetch
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import useFetch from '../hooks/useFetch'
+import { Link } from 'react-router-dom'
 
 export default function Homepage() {
+  const { loading, error, data } = useFetch('http://localhost:1337/reviews')
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  console.log(data)
+
   return (
     <div>
-      homepage
+      {data.reviews.map(review => (
+        <div key={review.id} className="review-card">
+          <div className="rating">{review.rating}</div>
+          <h2>{review.title}</h2>
+          
+          <small>console list</small>
+
+          <p>{review.body.substring(0, 200)}...</p>
+          <Link to={`/details/${review.id}`}>Read more</Link>
+        </div>
+      ))}
     </div>
   )
 }
