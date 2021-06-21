1e006bd9bfb482cd2c3c8c89086b124be93aaf35
Shaun <shaun@thenetninja.co.uk>
2021-06-21 15:15:33 +0100

    lesson-12

--- frontend/src/components/SiteHeader.js
+++ frontend/src/components/SiteHeader.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useQuery, gql } from '@apollo/client'
+
+const CATEGORIES = gql`
+  query GetCategories {
+    categories {
+      name,
+      id
+    }
+  }
+`
 
 export default function SiteHeader() {
+  const { loading, error, data } = useQuery(CATEGORIES)
+
+  if (loading) return <p>Loading categories...</p>
+  if (error) return <p>Error fetching categories</p>
+
   return (
     <div className="site-header">
       <Link to="/"><h1>Ninja Reviews</h1></Link>
+      <nav className="categories">
+        <span>Filter reviews by category:</span>
+        {data.categories.map(category => (
+          <Link key={category.id} to={`/category/${category.id}`}>
+            {category.name}
+          </Link>
+        ))}
+      </nav>
     </div>
   )
 }
